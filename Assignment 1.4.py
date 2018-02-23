5d122fb4ac7b52208d48147c96e3a2c98b3da771
shikhavaidh <36124149+shikhavaidh@users.noreply.github.com>
2018-02-23 21:23:14 +0530

    Assignment-1.4
    Assignment-1.4

---
+++ Assignment 1.4.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[23]:
+
+diameter=12
+radius=diameter/2
+print(radius)
+volume=(4/3)*(22/7)*radius*radius*radius
+print("Volume of sphere is - " + str(round(volume,3)))
+
+
+# In[ ]:
+
+
+
